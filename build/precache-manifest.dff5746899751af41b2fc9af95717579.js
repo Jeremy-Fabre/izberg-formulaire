90e3fcfd8a606ea87417fbb6b2b0330bba5a8305
JeF <47348544+Jeremy-Fabre@users.noreply.github.com>
2019-03-11 02:49:14 +0100

    Add files via upload

---
+++ build/precache-manifest.dff5746899751af41b2fc9af95717579.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "33effdd4b8c687a52a71",
+    "url": "/Jeremy-Fabre/izberg-formulaire/static/js/runtime~main.33effdd4.js"
+  },
+  {
+    "revision": "10ac782e733397380e5f",
+    "url": "/Jeremy-Fabre/izberg-formulaire/static/js/main.10ac782e.chunk.js"
+  },
+  {
+    "revision": "8fee606b0454f1eed12c",
+    "url": "/Jeremy-Fabre/izberg-formulaire/static/js/2.8fee606b.chunk.js"
+  },
+  {
+    "revision": "10ac782e733397380e5f",
+    "url": "/Jeremy-Fabre/izberg-formulaire/static/css/main.fdde1878.chunk.css"
+  },
+  {
+    "revision": "96e3dac1b2aa43625ec82529be22fe38",
+    "url": "/Jeremy-Fabre/izberg-formulaire/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3519e0ff001c57a34ead879aebe203da.js"
+  "/Jeremy-Fabre/izberg-formulaire/precache-manifest.dff5746899751af41b2fc9af95717579.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Jeremy-Fabre/izberg-formulaire/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
